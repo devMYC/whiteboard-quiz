78086b1c1040ec9d2b3bd295c1c9979d328b7f6d
Martin <mycha0@hotmail.com>
2017-12-12 19:40:43 +0800

    added quiz: stack

---
+++ src/stack/stack.spec.ts
@@ -0,0 +1,30 @@
+import 'jest'
+import Stack from './stack'
+
+
+test('should return the current stack size', () => {
+  expect.assertions(1)
+  const stack = new Stack()
+  expect( stack.size ).toBe(0)
+})
+
+
+test('should push new entries into the stack', () => {
+  expect.assertions(1)
+  const stack = new Stack()
+  stack.push('A')
+  stack.push('B')
+  expect( stack.size ).toBe(2)
+})
+
+
+test('should pop entries from the top of the stack', () => {
+  expect.assertions(3)
+  const stack = new Stack()
+  stack.push('A')
+  stack.push('B')
+  stack.push('C')
+  expect( stack.pop() ).toBe('C')
+  expect( stack.pop() ).toBe('B')
+  expect( stack.size ).toBe(1)
+})
---
+++ src/stack/stack.ts
@@ -0,0 +1,35 @@
+/**
+ * Implement a stack structure
+ *
+ * @example
+ *  const s = new Stack()
+ *  s.push('A')
+ *  s.push('B')
+ *  s.size -> 2
+ *  s.pop() -> 'B'
+ *  s.size -> 3
+ */
+export default class Stack<T> {
+
+  private entries: T[]
+
+  constructor() {
+    this.entries = []
+  }
+
+  public get size() {
+    return this.entries.length
+  }
+
+  public pop() {
+    const top: number = this.entries.length - 1
+    const popped: T = this.entries[top]
+    this.entries = this.entries.slice(0, top)
+    return popped
+  }
+
+  public push(entry: T) {
+    this.entries = this.entries.concat(entry)
+  }
+
+}
