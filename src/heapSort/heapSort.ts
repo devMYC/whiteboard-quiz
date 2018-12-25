dbc484623a7ce9701ddaa767fec42d7f31bb33be
Martin <mycha0@hotmail.com>
2018-12-25 14:19:03 +0800

    func signature fix

--- src/heapSort/heapSort.ts
+++ src/heapSort/heapSort.ts
@@ -18,42 +18,42 @@ type CmpFunc<T> = (a: T, b: T) => Compare
 
 export function heapSort<T>(fn?: CmpFunc<T>|null): (arr: T[]) => T[]
 export function heapSort<T>(fn: CmpFunc<T>|null|undefined, arr: T[]): T[]
-export function heapSort<T>(fn: CmpFunc<T>|null|undefined, arr: T[]): T[] {
+export function heapSort<T>(fn: CmpFunc<T>|null|undefined, arr?: T[]): any {
   fn == null && ( fn = cmp ) // tslint:disable-line triple-equals
   if ( arguments.length < 2 ) {
     return heapSort.bind(null, fn)
   }
 
-  let i = Math.floor(arr.length / 2 - 1)
+  let i = Math.floor((arr as T[]).length / 2 - 1)
   let c: number
   let r: number
 
   // heapify
   for ( ; i >= 0; i-- ) {
-    for ( r = i; r * 2 + 1 < arr.length; r = c ) {
+    for ( r = i; r * 2 + 1 < (arr as T[]).length; r = c ) {
       c = r * 2 + 1
-      c < arr.length - 1 && fn(arr[c], arr[c + 1]) < Compare.EQUAL && c++
-      if ( fn(arr[r], arr[c]) >= Compare.EQUAL ) {
+      c < (arr as T[]).length - 1 && fn((arr as T[])[c], (arr as T[])[c + 1]) < Compare.EQUAL && c++
+      if ( fn((arr as T[])[r], (arr as T[])[c]) >= Compare.EQUAL ) {
         break
       }
-      swap(arr, r, c)
+      swap((arr as T[]), r, c)
     }
   }
 
   // sort
-  for ( i = arr.length - 1; i > 0; i-- ) {
-    swap(arr, 0, i)
+  for ( i = (arr as T[]).length - 1; i > 0; i-- ) {
+    swap((arr as T[]), 0, i)
     for ( r = 0; r * 2 + 1 < i; r = c ) {
       c = r * 2 + 1
-      c < i - 1 && fn(arr[c], arr[c + 1]) === Compare.LESS && c++
-      if ( fn(arr[r], arr[c]) >= Compare.EQUAL ) {
+      c < i - 1 && fn((arr as T[])[c], (arr as T[])[c + 1]) === Compare.LESS && c++
+      if ( fn((arr as T[])[r], (arr as T[])[c]) >= Compare.EQUAL ) {
         break
       }
-      swap(arr, r, c)
+      swap((arr as T[]), r, c)
     }
   }
 
-  return arr
+  return (arr as T[])
 }
 
 function cmp<T>(a: T, b: T): Compare {
